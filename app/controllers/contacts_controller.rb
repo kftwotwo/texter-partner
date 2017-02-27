7d3e8ab967a9297c4583425200a65f6e23358fef
Kevin Finley and Margie Nevarez <kevin+margie@>
2017-02-27 10:47:00 -0800

    add contacts crud functionality in controller

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,49 @@
+class ContactsController < ApplicationController
+  def index
+    @contacts = Contact.all
+  end
+
+  def show
+    @contact = Contact.find(params[:id])
+  end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      flash[:notice] = "Saved"
+      redirect_to contact_path(@contact)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @contact = Contact.find(params[:id])
+  end
+
+  def update
+    @contact = Contact.find(params[:id])
+    if @contact.update(contact_params)
+      flash[:notice] = "Updated"
+      redirect_to contact_path(@contact)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @contact = Contact.find(params[:id])
+    @contact.destroy
+    redirect_to contacts_path
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :number)
+  end
+end
